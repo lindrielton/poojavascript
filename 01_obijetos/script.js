a94b10be2500ebddaaf334bad1e343cbb4242576
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-01-21 22:22:48 -0300

    pooexercicios

---
+++ 01_obijetos/script.js
@@ -0,0 +1,17 @@
+let cachorro = {
+    latir: function(){
+        console.log('au,au');
+    },
+    moder: function(){
+        console.log("mordeu");
+    },
+    
+    raca: function(){
+        console.log("labrador");
+    }
+}
+
+
+cachorro.latir();
+cachorro.moder();
+cachorro.raca();
