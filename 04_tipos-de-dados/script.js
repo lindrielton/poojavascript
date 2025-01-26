972fbea1f1d4f27a5074a519861af6dcd7ae85f9
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-01-25 22:40:08 -0300

    propiedades

---
+++ 04_tipos-de-dados/script.js
@@ -0,0 +1,12 @@
+let cavalo = {
+   patas: 4,
+   'foi domesticado':false,
+   foiCastrado: true,
+   n:'está no tempo de doma'
+}
+
+const m = 'patas';
+const a = 'foiCastrado';
+
+console.log(cavalo[m]);
+console.log(cavalo[a]);
