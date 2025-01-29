fc646c3eb28623322eaa45d501d74fda566b6b47
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-01-28 22:27:24 -0300

    obijetos

---
+++ 08-obijetos-dentro-de-obj/script.js
@@ -0,0 +1,28 @@
+
+
+
+let pessoa = {
+   idade:20,
+   nome:"maycon",
+   passaTempo:'futebol',
+   passarDosanos(){
+      this.idade +=1;
+     
+   },
+   caracteristicas:{
+      cnh:true,
+      descricao:["negro", "cabelo-liso", "olhos castanhos", 40]
+   }
+}
+
+
+
+
+console.log(pessoa.caracteristicas.descricao);
+console.log(pessoa.caracteristicas.cnh);
+
+
+
+
+
+
