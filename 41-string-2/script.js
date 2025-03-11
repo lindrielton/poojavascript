4f01d79b281f6e82390f1f215669f0c322e89109
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-03-11 05:29:59 -0300

    aprendendo metodos-str

---
+++ 41-string-2/script.js
@@ -0,0 +1,7 @@
+// para strings! 
+let est = 'paralelepipedo';
+
+console.log(est.toUpperCase());
+console.log(est.toLowerCase());
+console.log(est.indexOf('i'));
+console.log(est.charAt(10));
---
+++ 42-string-3/script.js
@@ -0,0 +1,5 @@
+// para string! 
+let frase = 'o rato roeu a ropa do rei de roma';
+
+console.log(frase.slice(2,6));
+console.log(frase.split(" "))
