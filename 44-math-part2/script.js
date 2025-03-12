12dc224aea5521f1761b732c4a11f12d969a2ec5
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-03-11 22:28:59 -0300

    date

---
+++ 44-math-part2/script.js
@@ -0,0 +1,7 @@
+// math - part2
+
+console.log(Math.pow(12,2));//potencia
+console.log(Math.ceil(12.345));//arendonda para cima
+console.log(Math.floor(12.345));//aredonda para baixo
+console.log(Math.sqrt(16));//mostra a raiz quadrada
+
---
+++ 45-date/script.js
@@ -0,0 +1,6 @@
+//date
+
+console.log(new Date());
+console.log(new Date(2020,4,30));
+console.log(new Date(12345678))
+
